b8c04907c268deec5ba34c759d056d54fecb4b44
Juan Riccetto <juanmartinre@gmail.com>
2021-09-01 10:22:44 -0300

    comentarios-bibiloteca-ubicación

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -9,7 +9,7 @@ namespace SRP
         public string Autor { get ; }
         public string Codigo { get ;  }  
         public string SectorBiblioteca { get ; set; } // La clase Libro solo debería poseer las características del mismo
-        public string EstanteBiblioteca { get ; set; } // Se deberá crear una nueva clase para la parte de biblioteca y otra para ubicación
+        public string EstanteBiblioteca { get ; set; } // Se deberá crear una nueva clase para la parte de biblioteca, que guarde los datos de los libros y otra para ubicación de estos
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
