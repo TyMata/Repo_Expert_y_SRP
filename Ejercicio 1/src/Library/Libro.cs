c1349f41b4483f1d6d460daa9da2ca6f632aff69
Juan Riccetto <juanmartinre@gmail.com>
2021-09-01 10:16:36 -0300

    Comentarios_nuevos

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -7,10 +7,9 @@ namespace SRP
 
         public string Titulo { get ; }
         public string Autor { get ; }
-        public string Codigo { get ;  }
+        public string Codigo { get ;  }  
         public string SectorBiblioteca { get ; set; } // La clase Libro solo debería poseer las características del mismo
-        public string EstanteBiblioteca { get ; set; } // Se deberá crear una nueva clase para la estantería
-
+        public string EstanteBiblioteca { get ; set; } // Se deberá crear una nueva clase para la parte de biblioteca y otra para ubicación
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
